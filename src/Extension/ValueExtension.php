5617a15b235611484d3bc443e012d9e358f36d14
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-05 12:30:50 +0300

    Code style fixes

---
+++ src/Extension/ValueExtension.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-value
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-value
+ */
+namespace Vain\Value\Extension;
+
+use Vain\Core\Extension\AbstractExtension;
+
+/**
+ * Class ValueExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class ValueExtension extends AbstractExtension
+{
+}
